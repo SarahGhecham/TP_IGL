dd06f3505b51e7f2c407813aba11a1dad7527a79
Sarah <ms_ghecham@esi.dz>
2024-12-26 02:35:34 +0100

    HomePageRight

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card'
 import { NavbarComponent } from '../../shared/navbar/navbar.component';
 import { MatIconModule } from '@angular/material/icon';
-
+import { CalendarComponent } from '../../calendar/calendar.component';
 
 
 @Component({
   selector: 'app-home',
-  imports: [MatCardModule,NavbarComponent,MatIconModule],
+  imports: [MatCardModule,NavbarComponent,MatIconModule,CalendarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
