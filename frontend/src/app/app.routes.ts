a8ad365b991a9f91cfedaca611635b48795b0df8
Sarah <ms_ghecham@esi.dz>
2024-12-29 11:33:25 +0100

    users/medecin + shared/calendar

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,12 +1,12 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
+import { HomeComponent } from './pages/users/medecin/home/home.component';
 import { ExamenTrendsComponent } from './pages/examen-trend/examen-trend.component';
 
 //import { SignInComponent } from './pages/sign-in/sign-in.component';
 export const routes: Routes = [
   {
     path: '',
-    component: ExamenTrendsComponent,
+    component: HomeComponent,
   },
   /*{path:'signIn',
     component:SignInComponent,
