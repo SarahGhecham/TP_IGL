91c4576dcc750d039d82c8b72b08b73b45e684da
HADJADJDAOUD <daoudhdj@gmail.com>
2024-12-29 15:09:51 +0100

    Save changes before switching branches

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,12 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-import { SignInComponent } from './pages/sign-in/sign-in.component';
+
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
   },
-  {path:'signIn',
-    component:SignInComponent,
-  },
+
+ 
 ];
