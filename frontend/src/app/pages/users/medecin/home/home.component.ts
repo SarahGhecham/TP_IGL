12a8edc17ca39837294a94eedde657c9c496c01e
HADJADJDAOUD <daoudhdj@gmail.com>
2024-12-30 19:42:05 +0100

    Save changes before switching branches

---
+++ frontend/src/app/pages/users/medecin/home/home.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card'
+import { NavbarComponent } from '../../../../shared/navbar/navbar.component';
+import { MatIconModule } from '@angular/material/icon';
+
+
+
+@Component({
+  selector: 'app-home',
+  imports: [MatCardModule,NavbarComponent,MatIconModule,CalendarComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+
+}
