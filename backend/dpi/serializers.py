3ebac159384d060f5a084f5d1e4745a2a6a61f81
bahaagueroumi <mm_gueroumi@esi.dz>
2025-01-01 18:19:25 +0100

    Update serializers.py

--- backend/dpi/serializers.py
+++ backend/dpi/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from api.models import Consultation,Soin,ResultatExamenImagerie,CompteRendu,DPI,Infirmier
+from api.models import Consultation,Soin,ResultatExamenImagerie,CompteRendu,DPI,Infirmier,BilanRadiologique
 
 
 class SoinSerializer(serializers.ModelSerializer):
@@ -26,27 +26,29 @@ class ConsultationSerializer(serializers.ModelSerializer):
         model = Consultation
         fields = ['id', 'dpi', 'date_consultation', 'motif', 'resume']
 
+from rest_framework import serializers
+
 class CompteRenduSerializerForGet(serializers.ModelSerializer):
     class Meta:
         model = CompteRendu
         fields = ['texte', 'date_creation']
 
+
 class ResultatExamenImagerieSerializerForGet(serializers.ModelSerializer):
-    compte_rendu = CompteRenduSerializerForGet(read_only=True)  # Le compte-rendu est un champ de lecture seule
+    compte_rendu = CompteRenduSerializerForGet(read_only=True)  # Compte-rendu is read-only
 
     class Meta:
         model = ResultatExamenImagerie
-        fields = ['dpi', 'radiologue', 'image', 'commentaire', 'date_examen', 'compte_rendu']
+        fields = ['id', 'radiologue', 'image', 'commentaire', 'date_examen', 'compte_rendu']
 
 
 class ResultatExamenImagerieSerializer(serializers.ModelSerializer):
-    dpi = serializers.PrimaryKeyRelatedField(queryset=DPI.objects.all())  
     radiologue = serializers.StringRelatedField() 
     compte_rendu = serializers.StringRelatedField() 
 
     class Meta:
         model = ResultatExamenImagerie
-        fields = ['id', 'dpi', 'radiologue', 'image', 'commentaire', 'date_examen', 'compte_rendu']
+        fields = ['id', 'radiologue', 'image', 'commentaire', 'date_examen', 'compte_rendu']
 
 
 class CompteRenduSerializer(serializers.ModelSerializer):
@@ -54,3 +56,17 @@ class CompteRenduSerializer(serializers.ModelSerializer):
         model = CompteRendu
         fields = ['id', 'texte', 'date_creation']
 
+
+class BilanRadiologiqueSerializer(serializers.ModelSerializer):
+    resultats = ResultatExamenImagerieSerializer(many=True, read_only=False)  # Nested serializers to create resultats
+
+    class Meta:
+        model = BilanRadiologique
+        fields = ['id', 'consultation', 'date_bilan', 'comment', 'resultats']
+
+    def create(self, validated_data):
+        resultats_data = validated_data.pop('resultats', [])
+        bilan_radiologique = BilanRadiologique.objects.create(**validated_data)
+        for resultat_data in resultats_data:
+            ResultatExamenImagerie.objects.create(bilan=bilan_radiologique, **resultat_data)
+        return bilan_radiologique
